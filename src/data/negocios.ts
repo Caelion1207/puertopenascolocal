2f908d5c21625207ec29f76cbbc8272744fe2371
Caelion1207 <rgbzkmf7cw@privaterelay.appleid.com>
2026-02-28 13:15:09 -0700

    Crear negocios.ts

---
+++ src/data/negocios.ts
@@ -0,0 +1,18 @@
+export const negocios = [
+  {
+    id: "tienda-ropa-001",
+    nombre: "Tienda de Ropa",
+    categoria: "Servicios",
+    descripcion: "Ropa de buena calidad",
+    direccion: "R Cortinez 118, Enrique de Tena",
+    telefono_contacto: "526381123970",
+    galeria: [
+      "/ropa1.jpg",
+      "/ropa2.jpg",
+      "/ropa3.jpg",
+      "/ropa4.jpg",
+      "/ropa5.jpg"
+    ],
+    tags: ["ropa", "moda", "tienda"]
+  }
+];
